87496854b24618d9434fad54ee05ab6f1beeb25d
kumu <kumu@2dfire.com>
2017-03-14 13:54:24 +0800

    inflate

--- layoutInflater/src/main/java/com/chilaim/android/sample/inflater/MainActivity.java
+++ layoutInflater/src/main/java/com/chilaim/android/sample/inflater/MainActivity.java
@@ -6,58 +6,84 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     LayoutInflater inflater;
-    LinearLayout viewGroup;
-
-    Button btnAddView;
+    LinearLayout root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewGroup = (LinearLayout) findViewById(R.id.activity_main);
+        root = (LinearLayout) findViewById(R.id.activity_main);
         inflater = LayoutInflater.from(this);
 
-        btnAddView = (Button) findViewById(R.id.btn_add_view);
     }
 
-    public void addView(View view) {
+    public void addView01(View view) {
         add01();
     }
 
-    //inflater.inflate(layoutId, null);
-    //inflater.inflate(layoutId, viewGroup, false);
-    //inflater.inflate(layoutId, viewGroup, true);
+    public void addView02(View view) {
+        add02();
+    }
+
+    public void addView03(View view) {
+        add03();
+    }
 
     /**
      * inflater.inflate(layoutId, null);
      */
     private void add01() {
-        // inflater.inflate(R.layout.inflater,viewGroup);
-        // View view = inflater.inflate(R.layout.inflater,viewGroup,false);
-        View view = inflater.inflate(R.layout.inflater, null);
-        //view.setLayoutParams();
+        View view = inflater.inflate(R.layout.inflater01, null);
         ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
         if (layoutParams == null) {
             Log.e("MainActivity", "layoutParams is null");
         } else {
             Log.e("MainActivity", "layoutParams width:height" + layoutParams.width + ":" + layoutParams.height);
         }
-        viewGroup.addView(view);
 
+        root.addView(view);
 
         //--------------------------
-        //viewGroup.addView(view,new ViewGroup.LayoutParams(-2,-2)); 相当于下面两行代码
+        //viewGroup.addView(view,new ViewGroup.LayoutParams(-2,-2));
+        //相当于下面两行代码
         // view.setLayoutParams(new ViewGroup.LayoutParams(-2,-2));
         //viewGroup.addView(view)
         //--------------------------
 
         //viewGroup.addView(view,new ViewGroup.LayoutParams(-2,-2));
 
+    }
+
+
+    /**
+     * inflater.inflate(layoutId, root, false);
+     */
+    private void add02() {
+        View view = inflater.inflate(R.layout.inflater01, root, false);
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        if (layoutParams == null) {
+            Log.e("MainActivity", "layoutParams is null");
+        } else {
+            Log.e("MainActivity", "layoutParams width:height" + layoutParams.width + ":" + layoutParams.height);
+        }
+        root.addView(view);
+    }
+
+    /**
+     * inflater.inflate(layoutId, root, true);
+     */
+    private void add03() {
+        View view = inflater.inflate(R.layout.inflater01, root, true);
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        if (layoutParams == null) {
+            Log.e("MainActivity", "layoutParams is null");
+        } else {
+            Log.e("MainActivity", "layoutParams width:height" + layoutParams.width + ":" + layoutParams.height);
+        }
     }
 }
