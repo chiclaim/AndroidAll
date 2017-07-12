f1e3b601999907f32f8276aa595b607743248334
kumu <kumu@2dfire.com>
2017-07-12 20:05:37 +0800

    [add] notification Activity press and back to main activity /  notification Activity launchMode(singleInstance,singleTask)

--- notification/src/main/java/sample2/module/chiclaim/com/notification/MainActivity.java
+++ notification/src/main/java/sample2/module/chiclaim/com/notification/MainActivity.java
@@ -3,7 +3,9 @@ package sample2.module.chiclaim.com.notification;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * android:launchMode="singleInstance"和singleTask
@@ -14,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String from = getIntent().getStringExtra("from");
+        if (!TextUtils.isEmpty(from)) {
+            Toast.makeText(this, from, Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void goSecondActivity(View view) {
--- notification/src/main/java/sample2/module/chiclaim/com/notification/SecondActivity.java
+++ notification/src/main/java/sample2/module/chiclaim/com/notification/SecondActivity.java
@@ -2,12 +2,15 @@ package sample2.module.chiclaim.com.notification;
 
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Description：
@@ -29,8 +32,9 @@ public class SecondActivity extends AppCompatActivity {
     public void sendNotification(View view) {
         NotificationManager mNotifyMgr =
                 (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Intent intent = new Intent(this, MainActivity.class);
         PendingIntent contentIntent = PendingIntent.getActivity(
-                this, 0, new Intent(this, MainActivity.class), 0);
+                this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.mipmap.ic_launcher)
@@ -39,4 +43,54 @@ public class SecondActivity extends AppCompatActivity {
                         .setContentIntent(contentIntent);
         mNotifyMgr.notify(1, mBuilder.build());
     }
+
+    /**
+     * 实现  弹出notification然后把界面全部关掉，在点击通知栏的通知 进入SecondActivity ，然后按返回键回到MainActivity 的功能
+     */
+    public void sendNotificationForBack(View view) {
+        Toast.makeText(this, "关闭所有的界面，然后点击通知栏的通知,再按返回键", Toast.LENGTH_SHORT).show();
+        //sendNotificationForBack1();
+        //or
+        sendNotificationForBack2();
+    }
+
+
+    public void sendNotificationForBack1() {
+        Intent resultIntent = new Intent(this, SecondActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // 添加返回栈
+        stackBuilder.addParentStack(SecondActivity.class);
+        // 添加Intent到栈顶
+        stackBuilder.addNextIntent(resultIntent);
+        // 创建包含返回栈的pendingIntent
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("SecondActivity");
+        builder.setContentText("SecondActivity");
+        builder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(2, builder.build());
+    }
+
+    public void sendNotificationForBack2() {
+        Intent backIntent = new Intent(this, MainActivity.class);
+        backIntent.putExtra("from", "sendNotificationForBack2");
+        backIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        final PendingIntent pendingIntent = PendingIntent.getActivities(this, 0, new Intent[]{backIntent, intent}, PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("SecondActivity");
+        builder.setContentText("SecondActivity");
+        builder.setContentIntent(pendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(2, builder.build());
+    }
+
 }
