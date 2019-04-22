a3f40aacff32552e95a9dba1e4789edc2c6738f3
chiclaim <chiclaim@gmail.com>
2019-04-22 10:47:49 +0800

    [update] update framework module label

--- framework/src/main/java/com/chiclaim/framework/MainActivity.kt
+++ framework/src/main/java/com/chiclaim/framework/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.chiclaim.framework
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val start = System.currentTimeMillis()
+        println("start: $start")
+
+        val handler = Handler()
+        val message = Message.obtain(handler) {
+            val end = System.currentTimeMillis()
+            println("end: $end")
+            println("diff:" + (end - start))
+        }
+        handler.sendMessage(message)
     }
 }
