1e43cacbc00abe5f79c34612198c7cd73893af6c
chiclaim <chiclaim@gmail.com>
2019-09-25 19:58:45 +0800

    [add] class字节码分析

---
+++ jvm/src/Client.java
@@ -0,0 +1,22 @@
+import java.io.Serializable;
+
+@Deprecated
+public class Client implements Serializable {
+
+    @Deprecated
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.setUsername("Chiclaim");
+        System.out.println(client.getUsername());
+    }
+}
