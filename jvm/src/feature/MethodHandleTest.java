5d7a15bde36c41f9596c820a06e9244c3dce9ffc
chiclaim <chiclaim@gmail.com>
2019-09-28 15:29:56 +0800

    [add]method handle

---
+++ jvm/src/feature/MethodHandleTest.java
@@ -0,0 +1,36 @@
+package feature;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
+public class MethodHandleTest {
+    class GrandFather {
+        void thinking() {
+            System.out.println("i am grandfather");
+        }
+    }
+
+    class Father extends GrandFather {
+        void thinking() {
+            System.out.println("i am father");
+        }
+    }
+
+    class Son extends Father {
+        void thinking() {
+            try {
+                MethodType mt = MethodType.methodType(void.class);
+                MethodHandle mh = MethodHandles.lookup().findSpecial(GrandFather.class,
+                        "thinking", mt, getClass());
+                mh.invoke(this);
+            } catch (Throwable e) {
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        (new MethodHandleTest().new Son()).thinking();
+    }
+
+}
