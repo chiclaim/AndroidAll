5b855446f15c65559531671a13844f66bb29e4be
chiclaim <chiclaim@gmail.com>
2019-10-08 20:29:08 +0800

    [update] update variable name

--- jvm/src/class_load/NotInitialization02.java
+++ jvm/src/class_load/NotInitialization02.java
@@ -2,7 +2,7 @@ package class_load;
 
 public class NotInitialization02 {
     public static void main(String[] args) {
-        Son[] parents = new Son[10];
+        Son[] sons = new Son[10];
     }
 
 }
