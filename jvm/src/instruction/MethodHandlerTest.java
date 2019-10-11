a9772554d647cd61f31984ce7a8d34602a68c2c5
chiclaim <chiclaim@gmail.com>
2019-10-11 11:20:04 +0800

    [add] MethodHandler test

--- jvm/src/instruction/MethodHandlerTest.java
+++ jvm/src/instruction/MethodHandlerTest.java
@@ -3,6 +3,7 @@ package instruction;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.lang.reflect.Method;
 
 public class MethodHandlerTest {
 
@@ -12,14 +13,14 @@ public class MethodHandlerTest {
         }
     }
 
-    public static void test(Object obj) throws Throwable {
+    private static void test(Object obj) throws Throwable {
 
         // 描述方法的返回值，参数类型
         MethodType methodType = MethodType.methodType(void.class, String.class);
 
         // 在指定类中查找符合给定的方法名称、方法返回值、参数类型的方法
         // 因为调用的是成员方法，成员方法都有一个隐含参数，也就是方法的调用者，通过 bingTo 传递过去
-        MethodHandle methodHandle =  MethodHandles.lookup()
+        MethodHandle methodHandle = MethodHandles.lookup()
                 .findVirtual(obj.getClass(), "println", methodType)
                 .bindTo(obj);
 
@@ -27,11 +28,22 @@ public class MethodHandlerTest {
         methodHandle.invokeExact("chiclaim");
     }
 
+    private static void test2(Object obj) throws Throwable {
+        Class clazz = obj.getClass();
+        Method method = clazz.getDeclaredMethod("println", String.class);
+        method.invoke(obj, "chiclaim");
+    }
 
 
     public static void main(String[] args) throws Throwable {
+
         test(System.out);      // PrintStream
         test(new MyPrinter()); // MyPrinter
+
+        System.out.println();
+
+        test2(System.out);      // PrintStream
+        test2(new MyPrinter()); // MyPrinter
     }
 
 }
