bf7096c6a1f89c689a12fda397de17d36c65bf42
chiclaim <chiclaim@gmail.com>
2019-12-27 18:51:29 +0800

    [add] file/text upload

--- android-lib-retrofit/retrofit-sample/app/src/main/java/com/chiclaim/android/retrofit_sample/FileUploadActivity.kt
+++ android-lib-retrofit/retrofit-sample/app/src/main/java/com/chiclaim/android/retrofit_sample/FileUploadActivity.kt
@@ -18,29 +18,47 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
 import java.io.File
 
 
-/*
-
-    https://futurestud.io/tutorials/retrofit-2-how-to-upload-files-to-server
-
+/**
+ * 单文件、多文件上传、每个参数的意义
  */
 class FileUploadActivity : BaseActivity() {
 
     companion object {
         const val PICK_FILE_RESULT_CODE = 1
-        const val MAX_FILE_NUMBER = 1
+        const val MAX_FILE_NUMBER = 2
 
         // set your base url
         const val API_URL = "http://10.1.81.240:8080/"
 
+        const val MODE_SINGLE_FILE = 1
+        const val MODE_MULTIPLE_FILE_BY_MULTIPLE_BODY = 2
+        const val MODE_MULTIPLE_FILE_BY_LIST_PART = 3
+
+
+
+
     }
 
     private val files = hashMapOf<String, Uri>()
+
+    private val mode = MODE_MULTIPLE_FILE_BY_MULTIPLE_BODY
+
+
+    private val uploadService: FileUploadService by lazy {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(API_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(FileUploadService::class.java)
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,10 +76,29 @@ class FileUploadActivity : BaseActivity() {
                 return@setOnClickListener
             }
 
-            files.forEach {
-                Log.e("FileUploadActivity", it.value.path)
-                uploadFile(files[it.key]!!)
+            var singleFileUri: Uri? = null
+            files.forEach { entry: Map.Entry<String, Uri> ->
+                if (singleFileUri == null) {
+                    singleFileUri = entry.value
+                }
+                Log.e("FileUploadActivity", "file path : ${entry.value.path}")
             }
+
+            when (mode) {
+                MODE_SINGLE_FILE -> {
+                    singleFileUri?.let {
+                        uploadFile(it)
+                    }
+                }
+                MODE_MULTIPLE_FILE_BY_MULTIPLE_BODY -> {
+                    uploadFileListByMultipartBody()
+                }
+                MODE_MULTIPLE_FILE_BY_LIST_PART -> {
+                    uploadFileListByListPart()
+                }
+            }
+
+
         }
     }
 
@@ -110,38 +147,12 @@ class FileUploadActivity : BaseActivity() {
     }
 
 
-    private fun uploadFile(fileUri: Uri) {
-
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl(API_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val service: FileUploadService = retrofit.create(FileUploadService::class.java)
-
-
-        val file = File(fileUri.path)
-
-        val mineType = getMimeType(fileUri) ?: return
-
-        val fileRequestBody = RequestBody.create(MediaType.parse(mineType), file)
-
-        // 相当于 HTML 页面里面的 文件选择控件： <input type="file" name="file_1"/>
-        val filePart = MultipartBody.Part.createFormData("file_1", file.name, fileRequestBody)
-
-        // 创建 form 字段，相当于 HTML 页面里面的 <input type="text" name="description"/>
-        val formFieldPart = RequestBody.create(MultipartBody.FORM, "这是文件描述")
-
-        val call = service.upload(formFieldPart, filePart)
-
-        call.enqueue(object : Callback<ResponseBody> {
+    private fun execute(call: Call<ResponseBody>?) {
+        call?.enqueue(object : Callback<ResponseBody> {
             override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
                 Toast.makeText(applicationContext, "upload success", Toast.LENGTH_SHORT).show()
-
-                fileUri.path?.let {
-                    files.remove(it)
-                    text_file_path.setText(R.string.file_upload_prefix)
-                }
+                files.clear()
+                text_file_path.setText(R.string.file_upload_prefix)
             }
 
             override fun onFailure(call: Call<ResponseBody?>?, t: Throwable) {
@@ -150,13 +161,118 @@ class FileUploadActivity : BaseActivity() {
         })
     }
 
+    private fun uploadFile(fileUri: Uri) {
+        execute(singleFileCall(fileUri))
+    }
+
+    private fun uploadFileListByMultipartBody() {
+        val formFieldBody = RequestBody.create(MultipartBody.FORM, "通过 MultipartBody 多文件上传")
+        execute(uploadService.upload(formFieldBody, buildMultipartBody()))
+    }
+
+    private fun uploadFileListByListPart() {
+        val formFieldBody = RequestBody.create(MultipartBody.FORM, "通过 List<MultipartBody.Part> 多文件上传")
+        execute(uploadService.upload(formFieldBody, buildListPart()))
+    }
+
+    private fun singleFileCall(fileUri: Uri): Call<ResponseBody>? {
+
+        // 多媒体类型，例如： image/jpeg
+        val mineType = getMimeType(fileUri) ?: return null
+
+        val file = File(fileUri.path)
+
+        val fileRequestBody = RequestBody.create(MediaType.parse(mineType), file)
+
+        // 相当于 HTML 页面里面的 文件选择控件： <input type="file" name="file_1"/>
+        val filePart = MultipartBody.Part.createFormData("file_1", file.name, fileRequestBody)
+
+        // 创建 form 字段，相当于 HTML 页面里面的 <input type="text" name="description"/>
+        val formFieldPart = RequestBody.create(MultipartBody.FORM, "单文件上传")
+
+        return uploadService.upload(formFieldPart, filePart)
+    }
+
+
+    private fun buildMultipartBody(): MultipartBody {
+        val builder = MultipartBody.Builder()
+        var index = 0
+        files.forEach { entry: Map.Entry<String, Uri> ->
+            val uri = entry.value
+            val mineType = getMimeType(uri)
+            mineType?.let {
+                val file = File(uri.path)
+                val requestBody = RequestBody.create(MediaType.parse(it), file)
+                builder.addFormDataPart("file${++index}", file.name, requestBody)
+            }
+        }
+        // 方式 3
+//        builder.addFormDataPart("description0", "通过 List<MultipartBody.Part> 多文件上传 in build MultipartBody")
+//        builder.addPart(MultipartBody.Part.createFormData("description0", "通过 List<MultipartBody.Part> 多文件上传 in build MultipartBody"))
+        return builder.build()
+    }
+
+    private fun buildListPart(): List<MultipartBody.Part> {
+        val list = arrayListOf<MultipartBody.Part>()
+
+        // 方式 1
+        // 通过 @Part("description") description: RequestBody 注解的方式来传递普通 form 参数
+
+        // 方式 2 （不可以）
+        //val formFieldBody = RequestBody.create(MultipartBody.FORM, "通过 List<MultipartBody.Part> 多文件上传 in list")
+        //list.add(MultipartBody.Part.create(formFieldBody))
+
+        // 方式 3
+        list.add(MultipartBody.Part.createFormData("description0", "通过 List<MultipartBody.Part> 多文件上传 in list"))
+
+
+        var index = 0
+        files.forEach {
+            val fileUri = it.value
+            val mineType = getMimeType(fileUri)
+            mineType?.let {
+                val file = File(fileUri.path)
+                val fileRequestBody = RequestBody.create(MediaType.parse(mineType), file)
+                val filePart = MultipartBody.Part.createFormData("file_${++index}", file.name, fileRequestBody)
+                list.add(filePart)
+            }
+        }
+
+
+        return list
+    }
+
 
     interface FileUploadService {
+        /**
+         * 单文件上传
+         */
         @Multipart
         @POST("fileUpload")
         fun upload(
             @Part("description") description: RequestBody,
             @Part file: MultipartBody.Part
+        ): Call<ResponseBody>
+
+
+        /**
+         * 多文件上传（List<MultipartBody.Part>）
+         */
+        @Multipart
+        @POST("fileUpload")
+        fun upload(
+            @Part("description") description: RequestBody,
+            @Part parts: List<MultipartBody.Part>
+        ): Call<ResponseBody>
+
+
+        /**
+         * 多文件上传（MultipartBody）
+         */
+        @POST("fileUpload")
+        fun upload(
+            @Part("description") description: RequestBody,
+            @Body multipartBody: MultipartBody
         ): Call<ResponseBody>
     }
 
