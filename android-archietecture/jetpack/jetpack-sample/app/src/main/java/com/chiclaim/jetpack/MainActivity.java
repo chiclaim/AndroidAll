048028be5512ad07a87c922b459225493fb1290e
chiclaim <chiclaim@gmail.com>
2020-01-28 11:50:50 +0800

    jetpack first commit

---
+++ android-archietecture/jetpack/jetpack-sample/app/src/main/java/com/chiclaim/jetpack/MainActivity.java
@@ -0,0 +1,13 @@
+package com.chiclaim.jetpack;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
