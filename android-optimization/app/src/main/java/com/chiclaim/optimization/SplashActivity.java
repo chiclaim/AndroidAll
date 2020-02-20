674b3fc2f9ac63a75a3dbcb6f305f19fbedaae9c
chiclaim <chiclaim@gmail.com>
2020-02-20 15:24:53 +0800

    性能优化

---
+++ android-optimization/app/src/main/java/com/chiclaim/optimization/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.chiclaim.optimization;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
