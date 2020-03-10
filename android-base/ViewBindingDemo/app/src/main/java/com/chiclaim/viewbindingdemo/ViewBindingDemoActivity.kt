88bfc37d911be07c2666103a7c0ace145121d6e0
chiclaim <chiclaim@gmail.com>
2020-03-10 14:28:17 +0800

    [add] ViewBindingDemo

---
+++ android-base/ViewBindingDemo/app/src/main/java/com/chiclaim/viewbindingdemo/ViewBindingDemoActivity.kt
@@ -0,0 +1,20 @@
+package com.chiclaim.viewbindingdemo
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * desc: ViewBindingDemo
+ *
+ * Created by kumu@2dfire.com on 2020/3/9.
+ */
+class ViewBindingDemoActivity : AppCompatActivity() {
+
+    /*private lateinit var binding: ViewLayoutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ViewLayoutBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }*/
+}
