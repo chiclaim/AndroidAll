83108588a9cbb58939107819d9158a847609bee2
chiclaim <chiclaim@gmail.com>
2020-03-29 13:52:18 +0800

    [add] StatusView 支持多行文字

--- android-custom-view/CustomViewCourse/app/src/main/java/com/chiclaim/customview/view/StatusView.java
+++ android-custom-view/CustomViewCourse/app/src/main/java/com/chiclaim/customview/view/StatusView.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.text.Layout;
 import android.text.StaticLayout;
 import android.text.TextPaint;
@@ -19,7 +19,6 @@ import com.chiclaim.customview.R;
 public class StatusView extends View {
 
     private Paint paint = new Paint();
-    private Rect rect = new Rect();
 
     private int width, height;
     private int outCircleBorder;
@@ -28,9 +27,13 @@ public class StatusView extends View {
     private int circleColor;
     private int textColor;
     private int textAngle;
-    private String text;
+    private String rawText;
     private float textMargin;
+    private int normalTextLength;
 
+    private StringBuilder stringBuilder;
+    private StaticLayout textLayout;
+    private String showText;
 
 
     public StatusView(Context context) {
@@ -56,8 +59,9 @@ public class StatusView extends View {
         innerCircleBorder = typedArray.getDimensionPixelOffset(R.styleable.StatusView_inside_circle_border_width, 0);
         circlesGap = typedArray.getDimensionPixelOffset(R.styleable.StatusView_circles_gap, 0);
         circleColor = typedArray.getColor(R.styleable.StatusView_circle_color, 0);
-        text = typedArray.getString(R.styleable.StatusView_text);
-        textAngle = typedArray.getInt(R.styleable.StatusView_text_angle,25);
+        rawText = typedArray.getString(R.styleable.StatusView_text);
+        textAngle = typedArray.getInt(R.styleable.StatusView_text_angle, 25);
+        normalTextLength = typedArray.getInt(R.styleable.StatusView_line_normal_text_length, 3);
         textColor = typedArray.getColor(R.styleable.StatusView_textColor, 0);
         if (textColor == 0) {
             textColor = circleColor;
@@ -82,6 +86,24 @@ public class StatusView extends View {
         return 0;
     }
 
+    private String getShowText(int step) {
+        if (!TextUtils.isEmpty(showText)) {
+            return showText;
+        }
+        if (stringBuilder == null) {
+            stringBuilder = new StringBuilder(rawText.length() + rawText.length() / step);
+        }
+        stringBuilder.setLength(0);
+        for (int i = 0; i < rawText.length(); i += step) {
+            stringBuilder.append(rawText, i, Math.min(i + step, rawText.length()));
+            if (i + step >= rawText.length()) {
+                continue;
+            }
+            stringBuilder.append('\n');
+        }
+        return stringBuilder.toString();
+    }
+
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -91,40 +113,48 @@ public class StatusView extends View {
         setMeasuredDimension(width, height);
     }
 
-    private StaticLayout textLayout;
-
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        // outside circle
+        // draw outside circle
         paint.setAntiAlias(true);
         paint.setColor(circleColor);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(outCircleBorder);
         canvas.drawCircle(width / 2f, height / 2f, (width - 2 * outCircleBorder) / 2.0f, paint);
 
-        // inside circle
+        // draw inside circle
         paint.setStrokeWidth(innerCircleBorder);
         float innerCircleRadius = (width - 2 * outCircleBorder - 2 * innerCircleBorder - 2 * circlesGap) / 2.0f;
         canvas.drawCircle(width / 2f, height / 2f, innerCircleRadius, paint);
 
         canvas.save();
 
-        // text
-        if (!TextUtils.isEmpty(text)) {
+        // draw text
+        if (!TextUtils.isEmpty(rawText)) {
+            float textSize = (innerCircleRadius * 2 - textMargin * 2) / normalTextLength;
+            float marginOffset = 0;
+            if (rawText.length() > normalTextLength) {
+                marginOffset = textSize / 2;
+                showText = getShowText(normalTextLength - 1);
+            } else {
+                showText = rawText;
+            }
 
             if (textLayout == null) {
                 TextPaint textPaint = new TextPaint();
-                textPaint.setTextSize((innerCircleRadius * 2 - textMargin * 2) / text.length());
+                textPaint.setTextSize(textSize);
                 textPaint.setStyle(Paint.Style.FILL);
                 textPaint.setColor(textColor);
-                textLayout = new StaticLayout(text, textPaint, getWidth(), Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
+                textPaint.setAntiAlias(true);
+                textPaint.setFakeBoldText(true);
+                textLayout = new StaticLayout(showText, textPaint, getWidth(), Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
             }
 
             canvas.rotate(textAngle, width / 2f, height / 2f);
             canvas.translate(
-                    outCircleBorder + innerCircleBorder + circlesGap + textMargin,
+                    outCircleBorder + innerCircleBorder + circlesGap + textMargin + marginOffset,
                     height / 2f - textLayout.getHeight() / 2.f);
 
             textLayout.draw(canvas);
